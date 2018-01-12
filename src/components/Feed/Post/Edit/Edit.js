f7bc480e3ae98f6c55f8e317f08aeaf3438fa2e0
jameslemire <webdev.jameslemire@gmail.com>
2018-01-12 08:34:36 -0700

    JL master - Update boilerplate.

--- src/components/Feed/Post/Edit/Edit.js
+++ src/components/Feed/Post/Edit/Edit.js
@@ -3,15 +3,34 @@ import React, { Component } from 'react';
 import './Edit.css';
 
 export default class Edit extends Component {
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      text: props.text
+    };
+
+    this.onUpdate = this.onUpdate.bind( this );
+  }
+
+  updateText( value ) {
+    this.setState({ text: value });
+  }
+
+  onUpdate() {
+
+  }
+
   render() {
     return (
       <section className="Edit__parent">
 
-        <textarea className="Edit__textarea" value={ this.props.text } onChange={ () => '' }></textarea>
+        <textarea className="Edit__textarea" value={ this.state.text } onChange={ ( e ) => this.updateText( e.target.value ) }></textarea>
 
         <div className="Edit__controls">
           <button id="Edit__controls-update" 
-                  className="Edit__control-btn">
+                  className="Edit__control-btn"
+                  onClick={ this.onUpdate }>
             Update
           </button>
 
