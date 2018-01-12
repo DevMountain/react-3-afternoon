d9ac5c3275dde72aa7cf2a5197016258a49b9e02
jameslemire <webdev.jameslemire@gmail.com>
2018-01-12 09:33:35 -0700

    JL master - Update boilerplate.

--- src/components/Feed/Post/Edit/Edit.js
+++ src/components/Feed/Post/Edit/Edit.js
@@ -10,18 +10,23 @@ export default class Edit extends Component {
       text: props.text
     };
 
-    this.onUpdate = this.onUpdate.bind( this );
+    this.updatePost = this.updatePost.bind( this );
   }
 
   updateText( value ) {
     this.setState({ text: value });
   }
 
-  onUpdate() {
+  updatePost() {
+    const { text } = this.state;
+
 
   }
 
   render() {
+    const { id, updatePostFn, hideEdit } = this.props;
+    const { text } = this.state;
+
     return (
       <section className="Edit__parent">
 
@@ -30,13 +35,13 @@ export default class Edit extends Component {
         <div className="Edit__controls">
           <button id="Edit__controls-update" 
                   className="Edit__control-btn"
-                  onClick={ this.onUpdate }>
+                  onClick={ this.updatePost }>
             Update
           </button>
 
           <button id="Edit__controsl-cancel"
                   className="Edit__control-btn"
-                  onClick={ this.props.cancelFn }>
+                  onClick={ this.props.hideEdit }>
             Cancel
           </button>
         </div>
