ddf6a901097d5cb71e9d75f98b86229f85f2ee32
= <webdev.jameslemire@gmail.com>
2018-01-16 09:49:34 -0700

    JL solution - Remove API from solution branch.

--- server/routes/posts.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router();
-const postController = require('../controllers/post_controller');
-
-Router.get('/', postController.get);
-Router.put('/', postController.put);
-Router.post('/', postController.post);
-Router.delete('/', postController.delete);
-
-module.exports = Router;
